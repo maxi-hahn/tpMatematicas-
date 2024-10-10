977d2f698d3ec1430c304fc09ef3385d27f4dd44
Micael1889 <Ramellomicael2000@gmail.com>
2024-10-10 00:38:24 -0300

    v
    v

--- Estoyalpedo.c
+++ Estoyalpedo.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-int main() {
-    int num = 18,sumador = 0,asignador = num;
-
-    while (asignador > 0)
+int binario(int num){
+int sumador = 0,asignador = num;
+      while (asignador > 0)
     {sumador++;
      asignador = asignador / 2; } 
     int size[sumador];
@@ -13,14 +13,130 @@ int main() {
     {   size[it]= num % 2;
         num = num / 2; }
 
-   printf("El numero en binario es: ");
+    printf("El numero en binario es: ");
     for (int iter = 0; iter < sumador; iter++) {
         printf("%d", size[iter]);
     }
     printf("\n");
 
-    
-    
+}
+
+int octal(int num){
+int sumador = 0,asignador = num;
+      while (asignador > 0)
+    {sumador++;
+     asignador = asignador / 8; } 
+    int size[sumador];
+
+    for (int it = sumador - 1; it >= 0; it--)
+    {   size[it]= num % 8;
+        num = num / 8; }
+
+    printf("El numero en octal es: ");
+    for (int iter = 0; iter < sumador; iter++) {
+        printf("%d", size[iter]);
+    }
+    printf("\n");
+
+}
+
+int hexadecimal(int num){
+int sumador = 0,asignador = num;
+      while (asignador > 0)
+    {sumador++;
+     asignador = asignador / 16; } 
+    int size[sumador];
+
+    for (int it = sumador - 1; it >= 0; it--)
+    {   size[it]= num % 16;
+        num = num / 16; }
+
+    printf("El numero en hexadecimal es: ");
+    for (int iter = 0; iter < sumador; iter++) {
+        if ( 9 < size[iter] )
+        {
+            switch (size[iter])
+            {
+            case 10:
+                printf("A");
+                break;
+            case 11:
+                printf("B");
+            break;
+            case 12:
+                printf("C");
+            break;
+            case 13:
+                printf("D");
+            break;
+            case 14:
+                printf("E");
+            break;
+            case 15:
+                printf("F");
+            break;
+            default:
+                break;
+            }
+        }else{printf("%d", size[iter]);}
+        
+        
+    }
+    printf("\n");
+
+}
+
+int binarioDecimal(int decimal){
+    int sumador = 0,asignado = decimal,numero = 0;
+    while (decimal != 1)
+    {
+       if (decimal % 10 == 1)
+        { numero += pow(2,sumador);}
+        sumador++;
+        decimal = decimal / 10;
+       
+    }
+    numero += pow(2,sumador);
+    return printf("El numero binario %d en decimal es : %d \n",asignado,numero);
+}
+
+int octalDecimal( int decimal){
+       int sumador = 0,asignado = decimal,numero = 0;
+    while (decimal != 1)
+    {
+       if (decimal % 10 == 1)
+        { numero += pow(8,sumador);}
+        sumador++;
+        decimal = decimal / 10;
+       
+    }
+    numero += pow(2,sumador);
+    return printf("El numero octal %d en decimal es : %d \n",asignado,numero);
+}
+
+int hexaDecimal(int decimal){
+    int sumador = 0,asignado = decimal,numero = 0;
+    while (decimal != 1)
+    {
+       if (decimal % 10 == 1)
+        { numero += pow(16,sumador);}
+        sumador++;
+        decimal = decimal / 10;
+       
+    }
+    numero += pow(16,sumador);
+    return printf("El numero hexadecimal %d en decimal es : %d \n",asignado,numero);
+}
+
+int main() {
+    int num = 700 ,decimal = 10010,oct = 22, hexa = 12 ;
+    binario(num);
+    octal(num);
+    hexadecimal(num);
+
+    binarioDecimal(binario(num));
+    octalDecimal(octal(num));
+    hexaDecimal(hexadecimal(num));
     system("pause");
     return 0;
 }
